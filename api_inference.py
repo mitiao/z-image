6347bba996bf7e90a9e697974f1b908de3bf3e95
mitiao <mitiao@gmail.com>
2026-06-01 10:36:13 +0800

    Update

--- api_inference.py
+++ api_inference.py
@@ -148,7 +148,6 @@ def create_generation(req: GenerateRequest) -> Response:
         guidance_scale=req.guidance_scale,
     )
 
-
 if __name__ == "__main__":
     host = os.environ.get("HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", "8000"))
